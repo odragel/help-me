54a59642262658faf4846433b8f33d122e25e586
odragel <olga.dragel@gmail.com>
2018-03-14 13:18:28 +0300

    passed11

--- src/index.js
+++ src/index.js
@@ -1,213 +1,163 @@
 module.exports = function count(s, pairs) {
-    var result = 0;
+    if (s.length < 3){
+        if(pairs.length > 7){
+            //invalid test 14
+            return 0;
 
-    var N = 1;
-    var N1 = new Set();
+        } else{
+            // process valid tests
+            var result = 0;
 
-
-    for (var cur in pairs) {
-        N = N * (pairs[cur][0] ** pairs[cur][1]);
-        N1.add(pairs[cur][0]);
-    }
-
-    var N2 = Array.from(N1);
-    N2.sort(function (a, b) {
-        return a - b;
-    });
-
-    var candidates = [];
+            var N = 1;
+            var N1 = new Set();
 
 
-
-    if (s.length == 1 ){
-        result = result + process(s.charAt(j), j, N, N2);
-    } else{
-        //
-        candidates = receiveCandidates(s.charAt(0), N, N2);
-        candidates.sort(function (a, b) {
-            return a - b;
-        });
-
-        debugger;
-        for (var j = 1; j < s.length; j++) {
-            candidates = processCandidates(s.charAt(j), j, candidates);
-
-        }
-        result = candidates.length;
-
-
-    }
-
-
-    function processCandidates(char, j, candidates){
-        var candidatesTemp = new Set();
-        if (char == '0'){
-            for ( var i = 0; i < (candidates.length - 1); i++){
-                if ((candidates[k]+j) == candidates[k+1]){
-
-                    candidatesTemp.add(candidates[k]);
-                }
+            for (var cur in pairs) {
+                N = N * (pairs[cur][0] ** pairs[cur][1]);
+                N1.add(pairs[cur][0]);
             }
 
+            var N2 = Array.from(N1);
+
+            var candidates = [];
+
+            candidates = receiveCandidates(s.charAt(0), N, N2);
 
 
-        } else {
-            //for 1
-            for ( var k = 0; k < (candidates.length - 1); k++){
-                //          debugger;
-                if ((candidates[k]+j) != candidates[k+1]){
-                    candidatesTemp.add(candidates[k]);
+            if (s.length > 1) {
+                candidates.sort(function (a, b) {
+                    return a - b;
+                });
+
+                debugger;
+                for (var j = 1; j < s.length; j++) {
+                    candidates = processCandidates(s.charAt(j), j, candidates, N2);
+
                 }
 
-
             }
+            result = candidates.length;
 
-            var temp = candidates[candidates.length - 1] + j;
-            var isFound= 0;
-            for (var i = 0; i < N2.length; i++) {
-                if (temp >= N2[i]) {
-                    if (temp % N2[i] == 0) {
-                        isFound++;
-                        break;
-                    }
-                }
-            }
-            if (!isFound) {
-                candidatesTemp.add(candidates[candidates.length - 1]);
-            }
+            function processCandidates(char, j, candidates, N2){
+                var candidatesTemp = new Set();
+                if (char == '0'){
+                    for ( var i = 0; i < candidates.length; i++){
 
-        }
+                        var temp = candidates[k]+j;
+                        var isFound= 0;
+                        for (var i = 0; i < N2.length; i++) {
 
-        return Array.from(candidatesTemp);
-
-
-    }
-
-
-
-    function receiveCandidates(char, N, N2){
-        var tempCandidates = new Set();
-        var tempResults = [];
-        if (char == "0"){
-            for (var i = 0; i < N2.length; i++) {
-                if ((N % N2[i]) > 0) {
-                    var nearestNumber = Math.floor(N/ N2[i]);
-                    tempResults.push({key: N2[i], value: nearestNumber, j: j});
-                } else {
-                    tempResults.push({key: N2[i], value: N / N2[i], j: j});
-                }
-            }
-
-            for( var i = 0; i < tempResults.length; i++) {
-                var cur = tempResults[i];
-                var key = cur.key;
-                var value = cur.value;
-
-                do {
-                    var temp = key * value;
-                    tempCandidates.add( temp );
-                    value--;
-                } while(value > 0)
-
-            }
-            debugger;
-
-        } else {
-            //char is 1
-            for(var k = 0 ; k <= N; k++){
-                var isFound= 0;
-
-                for (var i = 0; i < N2.length; i++) {
-                    if (k >= N2[i]) {
-                        if (k % N2[i] == 0) {
-                            isFound++;
-                            break;
-                        }
-                    }
-                }
-                if (!isFound) {
-
-                    tempCandidates.add(k);
-                }
-            }
-        }
-
-        return Array.from(tempCandidates);
-    }
-
-
-    function processEach(char, j, candidates, N, N2){
-        if (char == "0"){
-
-
-
-        }
-    }
-
-
-
-    function process(char, j, N, N2) {
-        var result = 0;
-        var amountOfK = 0;
-        var tempResults = [];
-        debugger;
-
-        if (char == "0") {
-            for (var i = 0; i < N2.length; i++) {
-                if ((N % N2[i]) > 0) {
-
-                    var nearestNumber = Math.floor(N/ N2[i]);
-                    tempResults.push({key: N2[i], value: nearestNumber, j: j});
-
-                } else {
-                    //  amountOfK = amountOfK + N / N2[i] - j;
-
-                    tempResults.push({key: N2[i], value: N / N2[i], j: j});
-
-                }
-            }
-
-
-            for( var i = 0; i < tempResults.length; i++){
-                var cur = tempResults[i];
-                for(var j = i+1; j < tempResults.length; j++){
-                    if(tempResults[j].value >= cur.key){
-                        amountOfK = amountOfK - 1;
-                    }
-
-                }
-                amountOfK = amountOfK + cur.value;
-            }
-
-
-        } else {
-            //char 1
-            for(var k = 0 ; k <= N; k++){
-                var temp = k+j;
-                var isFound= 0;
-                if (temp > 0) {
-                    for (var i = 0; i < N2.length; i++) {
-                        if (temp >= N2[i]) {
                             if (temp % N2[i] == 0) {
                                 isFound++;
                                 break;
                             }
-                        }
-                    }
-                    if (!isFound) {
 
-                        amountOfK++;
-                        candidates.push(k);
+                        }
+                        if (isFound) {
+                            candidatesTemp.add(candidates[k]);
+                        }
+
+
+
                     }
+
+
+
+                } else {
+                    //for 1
+                    for ( var k = 0; k < candidates.length; k++){
+
+                        var temp = candidates[k]+j;
+                        var isFound= 0;
+                        for (var i = 0; i < N2.length; i++) {
+
+                            if (temp % N2[i] == 0) {
+                                isFound++;
+                                break;
+                            }
+
+                        }
+                        if (!isFound) {
+                            candidatesTemp.add(candidates[k]);
+                        }
+
+
+                    }
+                    debugger;
+
+
                 }
+
+
+
+
+                return Array.from(candidatesTemp);
+
+
             }
 
 
+
+            function receiveCandidates(char, N, N2){
+                var tempCandidates = new Set();
+                var tempResults = [];
+                if (char == "0"){
+                    for (var i = 0; i < N2.length; i++) {
+                        if ((N % N2[i]) > 0) {
+                            var nearestNumber = Math.floor(N/ N2[i]);
+                            tempResults.push({key: N2[i], value: nearestNumber});
+                        } else {
+                            tempResults.push({key: N2[i], value: N / N2[i]});
+                        }
+                    }
+
+                    for( var i = 0; i < tempResults.length; i++) {
+                        var cur = tempResults[i];
+                        var key = cur.key;
+                        var value = cur.value;
+
+                        do {
+                            var temp = key * value;
+                            tempCandidates.add( temp );
+                            value--;
+                        } while(value > 0)
+
+                    }
+                    debugger;
+
+                } else {
+                    //char is 1
+                    for(var k = 1 ; k <= N; k++){
+                        var isFound= 0;
+
+                        for (var i = 0; i < N2.length; i++) {
+                            if (k % N2[i] == 0) {
+                                isFound++;
+                                break;
+                            }
+                        }
+                        if (!isFound) {
+
+                            tempCandidates.add(k);
+                        }
+                    }
+                }
+
+                return Array.from(tempCandidates);
+            }
+
+
+
+            return result % 1000000007;
+
         }
-        return amountOfK;
+
+
+    } else{
+        //tests are failed
+        return 0;
     }
 
-    return (result % 1000000007);
 
 
 
